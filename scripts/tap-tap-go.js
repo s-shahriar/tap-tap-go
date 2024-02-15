1cc0115ca2f0f8b0a1abbc6f15d77e208dd42b4d
Syed Shahriar <50046492+s-shahriar@users.noreply.github.com>
2024-02-15 12:01:04 +0600

    Add files via upload

--- scripts/tap-tap-go.js
+++ scripts/tap-tap-go.js
@@ -1,4 +1,8 @@
 function continueGame(){
+    let score = 0;
+    let life = 5;
+    setValueOfElement("score-count", score);
+    setValueOfElement("life-count", life);
     const alphabet = getARandomAlphabet();
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
@@ -7,6 +11,21 @@ function continueGame(){
 
 function play(){
     elementToggler('home-screen', 'hide');
+    elementToggler('score-section', 'hide');
     elementToggler('play-ground', 'show');
     continueGame();
 }
+
+
+function gameFinish() {
+    elementToggler('home-screen', 'hide');
+    elementToggler('play-ground', 'hide');
+    elementToggler('score-section', 'show');
+    const score = parseInt(getValueOfElement('score-count'));
+    setValueOfElement('final-score', score);
+    const currentAlphabet = document.getElementById('current-alphabet').innerText;
+    removeBackgroundColorById(currentAlphabet);
+}
+
+
+document.addEventListener('keyup', gameLogic);
--- scripts/utility.js
+++ scripts/utility.js
@@ -11,20 +11,61 @@ function elementToggler(elementId, operation) {
   }
 }
 
-function getARandomAlphabet(){
-    const randomNumber = Math.floor(Math.random() * 26);
-    const alphabet = String.fromCharCode(97 + randomNumber);
-    return alphabet;
+function getARandomAlphabet() {
+  const randomNumber = Math.floor(Math.random() * 26);
+  const alphabet = String.fromCharCode(97 + randomNumber);
+  return alphabet;
 }
 
-function setBackgroundColorById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400');
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-400");
 }
 
-function removeBackgroundColorById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.remove('bg-orange-400');
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
+}
+
+function getValueOfElement(elementId) {
+  const alphabet = document.getElementById(elementId).innerText;
+  return alphabet;
+}
+
+function setValueOfElement(elementId, value) {
+  document.getElementById(elementId).innerText = value;
 }
 
 
+
+function gameLogic(event) {
+  const playerPressedKey = event.key;
+  if (event.key === 'Escape') gameFinish();
+
+  const alphabet = getValueOfElement("current-alphabet")
+    .toString()
+    .toLowerCase();
+
+  if (alphabet === playerPressedKey) {
+    score = parseInt(getValueOfElement("score-count"));
+    score ++;
+    setValueOfElement("score-count", score);
+
+    removeBackgroundColorById(playerPressedKey);
+
+    const alphabet = getARandomAlphabet();
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    setBackgroundColorById(alphabet);
+
+
+  } else {
+    let life = parseInt(getValueOfElement("life-count"));
+    if (life > 0) {
+      life--;
+      setValueOfElement("life-count", life);
+    } else {
+      gameFinish();
+    }
+  }
+}
